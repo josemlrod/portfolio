82596bc865d6f43778d02da18b3957d4ad7d17c2
josemlrod <mrjosemlrod@gmail.com>
2024-08-11 23:49:26 -0400

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      keyframes: {
+        "slide-down": {
+          from: {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
+          to: {
+            transform: "translateY(100%)",
+          },
+        },
+      },
+      animation: {
+        "slide-down": "slide-down 0.75s forwards",
+      },
+    },
+  },
+};
