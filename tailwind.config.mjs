a19d75369c2616fd25ad231b3d46cd2f86223315
josemlrod <mrjosemlrod@gmail.com>
2024-08-12 01:31:14 -0400

    continued work on intro text

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,6 @@
+import Animations from "open-props/src/animations";
+import * as Easing from "open-props/src/easing";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -13,9 +16,18 @@ export default {
             transform: "translateY(100%)",
           },
         },
+        "slide-up": {
+          from: {
+            transform: "translateY(20px)",
+          },
+          to: {
+            transform: "translateY(0px)",
+          },
+        },
       },
       animation: {
-        "slide-down": "slide-down 0.75s forwards",
+        "slide-down": "slide-down 5s forwards",
+        "slide-up": `slide-up .5s ${Easing["--ease-1"]}`,
       },
     },
   },
