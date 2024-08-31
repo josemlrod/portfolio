3ab83b0dabc671069dbf31e185ec3fe347d45164
josemlrod <mrjosemlrod@gmail.com>
2024-08-31 13:41:11 -0400

    Revert "add endpoint"
    This reverts commit 8b7aa442dda2f3dc52e93bc03a9dd6f45d644bc7.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "hybrid",
-  adapter: node({
-    mode: "standalone",
-  }),
 });
