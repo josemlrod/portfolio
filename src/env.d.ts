5d345c33f9df157ec7ac400e3c5e8cc9cee244c8
josemlrod <mrjosemlrod@gmail.com>
2024-08-31 00:05:27 -0400

    create send message endpoint

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly AWS_ACCESS_KEY_ID: string;
+  readonly AWS_SECRET_ACCESS_KEY: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
